b4bee6f4bde99941f6ce5f93b6228ea5380561e0
kandjiramata27@gmail.com <kandjiramata27@gmail.com>
2026-06-20 23:04:18 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -126,4 +126,76 @@ const counterObserver = new IntersectionObserver((entries) => {
 });
 
 
+
+/* FORMULAIRE CONTACT */
+
+
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+       
+        const nom = document.getElementById("nom");
+        const prenom = document.getElementById("prenom");
+        const email = document.getElementById("email");
+        const message = document.getElementById("message");
+
+        
+        document.getElementById("nomError").textContent = "";
+        document.getElementById("prenomError").textContent = "";
+        document.getElementById("emailError").textContent = "";
+        document.getElementById("messageError").textContent = "";
+
+        document.getElementById("successMessage").textContent = "";
+
+
+        let valide = true;
+
+        
+        if (nom.value.trim() === "") {
+            document.getElementById("nomError").textContent = "Le nom est obligatoire";
+            valide = false;
+        }
+
+        
+        if (prenom.value.trim() === "") {
+            document.getElementById("prenomError").textContent = "Le prénom est obligatoire";
+            valide = false;
+        }
+
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email.value.trim())) {
+            document.getElementById("emailError").textContent = "Email invalide";
+            valide = false;
+        }
+
+         
+    
+        
+        if (message. value.trim().length < 20) {
+            document.getElementById("messageError").textContent =
+            "Le message doit contenir au moins 20 caractères";
+            valide = false;
+        }
+
+        
+        if (valide) {
+
+            document.getElementById("successMessage").textContent =
+            "Merci ! Votre message a été envoyé avec succès.";
+           
+            contactForm.reset();
+        }
+
+    });
+
+}
+
+
    
